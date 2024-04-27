e0a568869593b0f17f3038964fc1e9e3a5054316
Niklas <57962465+niklasnieberler@users.noreply.github.com>
2024-04-27 11:30:36 +0200

    add more registerServer methods in ServerRegisterManager

--- src/main/kotlin/world/estaria/server/registration/ServerRegisterManager.kt
+++ src/main/kotlin/world/estaria/server/registration/ServerRegisterManager.kt
@@ -2,6 +2,7 @@ package world.estaria.server.registration
 
 import com.velocitypowered.api.proxy.ProxyServer
 import com.velocitypowered.api.proxy.server.ServerInfo
+import io.fabric8.kubernetes.api.model.Pod
 import java.net.InetSocketAddress
 import java.util.logging.Logger
 
@@ -14,12 +15,23 @@ class ServerRegisterManager(
     private val logger: Logger
 ) {
 
+    fun registerServer(pod: Pod) {
+        val podIp = pod.status.podIP
+        val podName = pod.metadata.name
+        registerServer(podName, podIp)
+    }
+
+    fun isServerRegistered(pod: Pod): Boolean {
+        val name = pod.metadata.name
+        return this.proxyServer.getServer(name).isPresent
+    }
+
     fun registerServer(serverInfo: ServerInfo) {
         this.proxyServer.registerServer(serverInfo)
         this.logger.info("Register new server ${serverInfo.name} on ${serverInfo.address}")
     }
 
-    fun registerServer(name: String, host: String, port: Int = 25565) {
+    private fun registerServer(name: String, host: String, port: Int = 25565) {
         val socketAddress = InetSocketAddress.createUnresolved(host, port)
         val serverInfo = ServerInfo(name, socketAddress)
         registerServer(serverInfo)
