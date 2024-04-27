355a0e9a4a1ecbd420ce3ee7933e6d419e16d3fa
Niklas <57962465+niklasnieberler@users.noreply.github.com>
2024-04-27 11:30:19 +0200

    fix pod registration

--- src/main/kotlin/world/estaria/server/registration/PodResourceEventHandler.kt
+++ src/main/kotlin/world/estaria/server/registration/PodResourceEventHandler.kt
@@ -2,7 +2,6 @@ package world.estaria.server.registration
 
 import io.fabric8.kubernetes.api.model.Pod
 import io.fabric8.kubernetes.client.informers.ResourceEventHandler
-import java.net.InetAddress
 
 /**
  * @author Niklas Nieberler
@@ -12,30 +11,23 @@ class PodResourceEventHandler(
     private val serverRegisterManager: ServerRegisterManager
 ) : ResourceEventHandler<Pod> {
 
-    private val hostAddress = InetAddress.getLocalHost().hostAddress
-
     override fun onAdd(pod: Pod) {
-        if (!hasPodAllRequirements(pod))
+        if (!PodRequirements.hasPodAllRequirements(pod))
             return
-        val podIp = pod.status.podIP
-        val podName = pod.metadata.name
-        this.serverRegisterManager.registerServer(podName, podIp)
+        this.serverRegisterManager.registerServer(pod)
     }
 
-    override fun onUpdate(oldObj: Pod, newObj: Pod) {}
+    override fun onUpdate(oldPod: Pod, newPod: Pod) {
+        if (!PodRequirements.hasPodAllRequirements(newPod))
+            return
+        if (this.serverRegisterManager.isServerRegistered(newPod))
+            return
+        this.serverRegisterManager.registerServer(newPod)
+    }
 
     override fun onDelete(pod: Pod, deletedFinalStateUnknown: Boolean) {
-        if (!hasPodAllRequirements(pod))
-            return
         val podName = pod.metadata.name
         this.serverRegisterManager.unregisterServer(podName)
     }
 
-    private fun hasPodAllRequirements(pod: Pod): Boolean {
-        if (pod.metadata.namespace != KUBERNETES_NAMESPACE)
-            return false
-        if (pod.status.podIP == this.hostAddress)
-            return false
-        return true
-    }
 }
